9fc14f28cc03870e0df1c1ea78f484ece1f96c3e
ShisukeUrahara <abhisheksagu@gmail.com>
2023-11-11 12:56:24 +0530

    Binary-Search-Tree-Assignment: Submit question 1

---
+++ BINARY_SEARCH_TREE/1_Max_Path_Sum_In_Binary_Tree.js
@@ -0,0 +1,64 @@
+/* 
+Maximum Path Sum in Binary Tree
+Please Complete Below assignment and upload github link of that solution after completion
+Problem Statement:
+Maximum Path Sum in Binary Tree
+
+Problem Statement:
+Write a function to find the maximum path sum in a binary tree.
+
+Input Format:
+The input for the maximum path sum function is a binary tree represented by its root node. The elements of the binary tree will be provided as a space-separated string.
+
+Output Format:
+The maximum path sum function should return the maximum sum of any path in the binary tree.
+
+Test Case 1:
+Sample Input: 1 2 3 -1 5 6 7
+
+Sample Output: 18
+
+Test Case 2:
+sample input: 1 2 null 3 null 4 null 5
+
+sample output: 15
+
+Level: Easy
+Hints:
+Use a recursive approach to calculate the maximum path sum. At each node, compute the maximum path sum through that node (including the node itself) by considering both the left and right subtrees.
+Update the global maximum path sum if a new maximum is found.
+Approach:
+Initialize a global variable maxSum to a very small value.
+Implement a helper function findMaxPathSum that takes a node as input and returns the maximum path sum through that node.
+In the findMaxPathSum function, if the node is null, return 0.
+Recursively calculate the maximum path sum in the left subtree and store it in leftSum.
+Recursively calculate the maximum path sum in the right subtree and store it in rightSum.
+Compute the maximum path sum through the current node by considering three options: node alone, node with left subtree, and node - with right subtree.
+Update the maxSum if the new path sum is greater.
+Return the maximum path sum through the current node.
+Call the findMaxPathSum function on the root node and store the result in maxSum.
+Return the maxSum as the final result.
+*/
+
+let maxSum = 0;
+
+const maxPathSumRecursive = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  let leftPathSum = Math.max(maxPathSumRecursive(root.left), 0);
+  let rightPathSum = Math.max(maxPathSumRecursive(root.right), 0);
+
+  maxSum = Math.max(maxSum, root.val + leftPathSum + rightPathSum);
+
+  return root.val + Math.max(leftPathSum, rightPathSum);
+};
+
+var maxPathSum = function (root) {
+  maxSum = -Infinity;
+
+  maxPathSumRecursive(root);
+
+  return maxSum;
+};
